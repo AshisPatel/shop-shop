82f3b27a8365a7a6b42671d09ec1677f16b98607
AP <ashis.n.patel@gmail.com>
2021-11-09 21:57:57 -0600

    Add persistence of cart between user's visits to the webpage

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
 import './style.css';
 
 import { useStoreContext } from "../../utils/GlobalState";
-import { TOGGLE_CART } from "../../utils/actions";
+import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
+import { idbPromise } from '../../utils/helpers';
 
 const Cart = () => {
     const [state, dispatch] = useStoreContext();
@@ -16,6 +17,27 @@ const Cart = () => {
             type: TOGGLE_CART
         });
     }
+
+    // check indexedDB's 'cart' store everytime the cart opens
+    // our dependency array includes all the items that the useEffect() hook is dependent upon, and only runs again (after mounting) if any of the items in the dependency array have changed
+    // in this case, that means that even if there is nothing to retrieve from the indexedDB store, the cart.length variable does not change from 0 and so the dependency array does not execute again
+    useEffect(() => {
+        async function getCart() {
+            // have to wait for the get request to complete before adding to the global state
+            const indexedCart = await idbPromise('cart', 'get');
+            console.log(`useEffect cart: ${indexedCart}`); 
+            dispatch({
+                type: ADD_MULTIPLE_TO_CART,
+                products: indexedCart
+            });
+        };
+        // if the globalState for the cart is empty, check to see if there are any objects in the cart indexDB store
+        // When the user closes the browser, the globalState is wiped and will be re-instantiated as blank
+        // Thus, if there was anything the last time they were shopping, it will be added to indexedDB and so when they open the website again, it'll populate the globalState using indexedDB
+        if(!cart.length) {
+            getCart();
+        }
+    }, [cart.length, dispatch]);
 
     const calculateTotal = () => {
         let sum = 0;
