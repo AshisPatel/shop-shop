05da8510a207a5a70c55f9dc0ad713f76156d48f
AshisPatel <ashis.n.patel@gmail.com>
2021-11-10 10:30:20 -0600

    Add query and resolverfor checkout using stripe

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,7 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Product, Category, Order } = require('../models');
 const { signToken } = require('../utils/auth');
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 const resolvers = {
   Query: {
@@ -50,6 +51,47 @@ const resolvers = {
       }
 
       throw new AuthenticationError('Not logged in');
+    },
+
+    checkout: async(parent, args, context) => {
+      // we pass our array of ids via args.products to create an instance of the Order model, which creates a purchaseDate(default: date.now) and the second paramter is the array of product id's which will be ObjectIds so mongoose can reference the Product models and build the full product from just the id.
+
+      const order = new Order({ products: args.products });
+      // here we use execPopulate because we are performing an operation on a specific instance of an Order model ('order')
+      const { products } = await order.populate('products').execPopulate();
+      const line_items = [];
+
+      for(let i=0; i< products.length; i++) {
+        // create product id by accessing the product object from the products variable => contains fully expanded data from the Product model
+        const product = await stripe.products.create({
+          name: products[i].name,
+          description: products[i].description
+        });
+
+        // create price id using the product id
+        const price = await stripe.prices.create({
+          product: product.id,
+          // price is stored in cents not dollars so we have to multiply our stored dollar value by 100
+          unit_amount: products[i].price * 100,
+          currency: 'usd'
+        });
+
+        // add price id to the line items array 
+        line_items.push({
+          price: price.id,
+          quantity: 1
+        });
+      }
+
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: line_items,
+        mode: 'payment',
+        success_url: 'https://example.com/success?session_ID={CHECKOUT_SESSION_ID}',
+        cancel_url: 'https://example.com/cancel'
+      });
+
+      return { session: session.id };
     }
   },
   Mutation: {
